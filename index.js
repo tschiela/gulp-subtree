f06debc539f19bbffc2f3529e0cdb0dc52700b9f
Thomas Schiela <thomas.schiela@gmail.com>
2015-02-01 12:50:30 +0100

    git need relativ path to dist folder

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ module.exports = function (options) {
       );
     }
 
-    var folder = file.path.replace(file.cwd + path.sep, '');
+    var folder = path.basename(file);
 
     var remote = 'origin';
     var branch = 'gh-pages';
@@ -41,17 +41,8 @@ module.exports = function (options) {
     // execute('git add ' + folder, function () {
     execute('git add ' + folder + ' && git commit -m "' + message + '"', function () {
       gutil.log('Temporarily committing ' + chalk.magenta(folder));
-      execute('git ls-remote ' + remote + ' ' + branch, function (rmt) {
-        if (rmt.length > 0) {
-          gutil.log('Cleaning ' + chalk.cyan(remote) + '/' + chalk.cyan(branch));
-          execute('git push ' + remote + ' :' + branch, function () {
-            deployFinish();
-          });
-        }
-        else {
-          deployFinish();
-        }
-      });
+
+      deployFinish();
     });
 
     //////////////////////////////
@@ -60,10 +51,7 @@ module.exports = function (options) {
     var deployFinish = function () {
       gutil.log('Pushing ' + chalk.magenta(folder) + ' to ' + chalk.cyan(remote) + '/' + chalk.cyan(branch));
       execute('git subtree push --prefix ' + folder + ' ' + remote + ' ' + branch, function () {
-        gutil.log('Resetting ' + chalk.magenta(folder) + ' temporary commit');
-        execute('git reset HEAD^', function () {
-          return cb(null, file);
-        });
+        return cb(null, file);
       });
     };
   });
